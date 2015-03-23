8e7b80cae03bb3031dd275170d537f9421fa6d2d
gBringout <gael.bringout@gmail.com>
2015-03-23 17:26:14 +0100

    Correct the allocation for SHC

--- idealFFL.m
+++ idealFFL.m
@@ -3,8 +3,8 @@ clear all
 
 %% Commun parameter to each fields
 
-maxOrder = 2; % maximal order for the spherical harmonics serie
-maxDegree = 2; % maximal order for the spherical harmonics serie
+maxOrder = 1; % maximal order for the spherical harmonics serie
+maxDegree = 1; % maximal order for the spherical harmonics serie
 rhoReference = 0.10;% [m] Fields are described inside a sphere of 1 meter of diameter
 
 %sometime, we cannot use zero, for examples when we want to display in a
@@ -20,12 +20,12 @@ coefDrive_Y = drivePeakAmplitude;
 
 %% Quadrupole 0
 disp('Defines the quadrupole 0')
-Quadru_0.bc(1).coefficient = ones(maxOrder,maxDegree)*amplitude_zero;
-Quadru_0.bc(2).coefficient = ones(maxOrder,maxDegree)*amplitude_zero;
-Quadru_0.bc(3).coefficient = ones(maxOrder,maxDegree)*amplitude_zero;
-Quadru_0.bs(1).coefficient = ones(maxOrder,maxDegree)*amplitude_zero;
-Quadru_0.bs(2).coefficient = ones(maxOrder,maxDegree)*amplitude_zero;
-Quadru_0.bs(3).coefficient = ones(maxOrder,maxDegree)*amplitude_zero;
+Quadru_0.bc(1).coefficient = ones(maxOrder+1,maxDegree+1)*amplitude_zero;
+Quadru_0.bc(2).coefficient = ones(maxOrder+1,maxDegree+1)*amplitude_zero;
+Quadru_0.bc(3).coefficient = ones(maxOrder+1,maxDegree+1)*amplitude_zero;
+Quadru_0.bs(1).coefficient = ones(maxOrder+1,maxDegree+1)*amplitude_zero;
+Quadru_0.bs(2).coefficient = ones(maxOrder+1,maxDegree+1)*amplitude_zero;
+Quadru_0.bs(3).coefficient = ones(maxOrder+1,maxDegree+1)*amplitude_zero;
 
 Quadru_0.bc(1).coefficient(2,2) = coefGradient;
 Quadru_0.bs(2).coefficient(2,2) = -coefGradient;
@@ -35,12 +35,12 @@ Quadru_0.rhoReference = rhoReference;
 
 %% Q45
 disp('Defines the quadrupole 45')
-Quadru_45.bc(1).coefficient = ones(maxOrder,maxDegree)*amplitude_zero;
-Quadru_45.bc(2).coefficient = ones(maxOrder,maxDegree)*amplitude_zero;
-Quadru_45.bc(3).coefficient = ones(maxOrder,maxDegree)*amplitude_zero;
-Quadru_45.bs(1).coefficient = ones(maxOrder,maxDegree)*amplitude_zero;
-Quadru_45.bs(2).coefficient = ones(maxOrder,maxDegree)*amplitude_zero;
-Quadru_45.bs(3).coefficient = ones(maxOrder,maxDegree)*amplitude_zero;
+Quadru_45.bc(1).coefficient = ones(maxOrder+1,maxDegree+1)*amplitude_zero;
+Quadru_45.bc(2).coefficient = ones(maxOrder+1,maxDegree+1)*amplitude_zero;
+Quadru_45.bc(3).coefficient = ones(maxOrder+1,maxDegree+1)*amplitude_zero;
+Quadru_45.bs(1).coefficient = ones(maxOrder+1,maxDegree+1)*amplitude_zero;
+Quadru_45.bs(2).coefficient = ones(maxOrder+1,maxDegree+1)*amplitude_zero;
+Quadru_45.bs(3).coefficient = ones(maxOrder+1,maxDegree+1)*amplitude_zero;
 
 Quadru_45.bs(1).coefficient(2,2) = coefGradient;
 Quadru_45.bc(2).coefficient(2,2) = coefGradient;
@@ -49,12 +49,12 @@ Quadru_45.current = 1;
 Quadru_45.rhoReference = rhoReference;
 %% Selection
 disp('Define the Selection coil')
-Selection_Z.bc(1).coefficient = ones(maxOrder,maxDegree)*amplitude_zero;
-Selection_Z.bc(2).coefficient = ones(maxOrder,maxDegree)*amplitude_zero;
-Selection_Z.bc(3).coefficient = ones(maxOrder,maxDegree)*amplitude_zero;
-Selection_Z.bs(1).coefficient = ones(maxOrder,maxDegree)*amplitude_zero;
-Selection_Z.bs(2).coefficient = ones(maxOrder,maxDegree)*amplitude_zero;
-Selection_Z.bs(3).coefficient = ones(maxOrder,maxDegree)*amplitude_zero;
+Selection_Z.bc(1).coefficient = ones(maxOrder+1,maxDegree+1)*amplitude_zero;
+Selection_Z.bc(2).coefficient = ones(maxOrder+1,maxDegree+1)*amplitude_zero;
+Selection_Z.bc(3).coefficient = ones(maxOrder+1,maxDegree+1)*amplitude_zero;
+Selection_Z.bs(1).coefficient = ones(maxOrder+1,maxDegree+1)*amplitude_zero;
+Selection_Z.bs(2).coefficient = ones(maxOrder+1,maxDegree+1)*amplitude_zero;
+Selection_Z.bs(3).coefficient = ones(maxOrder+1,maxDegree+1)*amplitude_zero;
 
 Selection_Z.bc(1).coefficient(2,2) = -coefGradient;
 Selection_Z.bs(2).coefficient(2,2) = -coefGradient;
@@ -64,12 +64,12 @@ Selection_Z.current = 1;
 Selection_Z.rhoReference = rhoReference;
 %% Drive X
 disp('Define the Drive X')
-Drive_X.bc(1).coefficient = ones(maxOrder,maxDegree)*amplitude_zero;
-Drive_X.bc(2).coefficient = ones(maxOrder,maxDegree)*amplitude_zero;
-Drive_X.bc(3).coefficient = ones(maxOrder,maxDegree)*amplitude_zero;
-Drive_X.bs(1).coefficient = ones(maxOrder,maxDegree)*amplitude_zero;
-Drive_X.bs(2).coefficient = ones(maxOrder,maxDegree)*amplitude_zero;
-Drive_X.bs(3).coefficient = ones(maxOrder,maxDegree)*amplitude_zero;
+Drive_X.bc(1).coefficient = ones(maxOrder+1,maxDegree+1)*amplitude_zero;
+Drive_X.bc(2).coefficient = ones(maxOrder+1,maxDegree+1)*amplitude_zero;
+Drive_X.bc(3).coefficient = ones(maxOrder+1,maxDegree+1)*amplitude_zero;
+Drive_X.bs(1).coefficient = ones(maxOrder+1,maxDegree+1)*amplitude_zero;
+Drive_X.bs(2).coefficient = ones(maxOrder+1,maxDegree+1)*amplitude_zero;
+Drive_X.bs(3).coefficient = ones(maxOrder+1,maxDegree+1)*amplitude_zero;
 
 %we set the current higher, in order to get a relatively small sensitivity
 Drive_X.current = 293.29;
@@ -78,12 +78,12 @@ Drive_X.bc(1).coefficient(1,1) = coefDrive_X/Drive_X.current;
 Drive_X.rhoReference = rhoReference;
 %% Drive Y
 disp('Define the Drive Y')
-Drive_Y.bc(1).coefficient = ones(maxOrder,maxDegree)*amplitude_zero;
-Drive_Y.bc(2).coefficient = ones(maxOrder,maxDegree)*amplitude_zero;
-Drive_Y.bc(3).coefficient = ones(maxOrder,maxDegree)*amplitude_zero;
-Drive_Y.bs(1).coefficient = ones(maxOrder,maxDegree)*amplitude_zero;
-Drive_Y.bs(2).coefficient = ones(maxOrder,maxDegree)*amplitude_zero;
-Drive_Y.bs(3).coefficient = ones(maxOrder,maxDegree)*amplitude_zero;
+Drive_Y.bc(1).coefficient = ones(maxOrder+1,maxDegree+1)*amplitude_zero;
+Drive_Y.bc(2).coefficient = ones(maxOrder+1,maxDegree+1)*amplitude_zero;
+Drive_Y.bc(3).coefficient = ones(maxOrder+1,maxDegree+1)*amplitude_zero;
+Drive_Y.bs(1).coefficient = ones(maxOrder+1,maxDegree+1)*amplitude_zero;
+Drive_Y.bs(2).coefficient = ones(maxOrder+1,maxDegree+1)*amplitude_zero;
+Drive_Y.bs(3).coefficient = ones(maxOrder+1,maxDegree+1)*amplitude_zero;
 
 %we set the current higher, in order to get a relatively small sensitivity
 Drive_Y.current = 379.71;
@@ -92,7 +92,7 @@ Drive_Y.bc(2).coefficient(1,1) = coefDrive_Y/Drive_Y.current;
 Drive_Y.rhoReference = rhoReference;
 
 %%
-clear('maxOrder','maxDegree','rhoReference',...
+clear('maxOrder+1','maxDegree+1','rhoReference',...
     'amplitude_zero','gradientOnTheLine','drivePeakAmplitude',...
     'coefGradient','coefDrive_X','coefDrive_Y')
 
--- idealFFP.m
+++ idealFFP.m
@@ -3,8 +3,8 @@ clear all
 
 %% Commun parameter to each fields
 
-maxOrder = 2; % maximal order for the spherical harmonics serie
-maxDegree = 2; % maximal order for the spherical harmonics serie
+maxOrder = 1; % maximal order for the spherical harmonics serie
+maxDegree = 1; % maximal order for the spherical harmonics serie
 rhoReference = 0.5;% [m] Fields are described inside a sphere of 1 meter of diameter
 
 %sometime, we cannot use zero, for examples when we want to display in a
@@ -21,12 +21,12 @@ coefDrive_Z = drivePeakAmplitude;
 
 %% Delection
 disp('Define the Selection coil')
-Selection_Z.bc(1).coefficient = ones(maxOrder,maxDegree)*amplitude_zero;
-Selection_Z.bc(2).coefficient = ones(maxOrder,maxDegree)*amplitude_zero;
-Selection_Z.bc(3).coefficient = ones(maxOrder,maxDegree)*amplitude_zero;
-Selection_Z.bs(1).coefficient = ones(maxOrder,maxDegree)*amplitude_zero;
-Selection_Z.bs(2).coefficient = ones(maxOrder,maxDegree)*amplitude_zero;
-Selection_Z.bs(3).coefficient = ones(maxOrder,maxDegree)*amplitude_zero;
+Selection_Z.bc(1).coefficient = ones(maxOrder+1,maxDegree+1)*amplitude_zero;
+Selection_Z.bc(2).coefficient = ones(maxOrder+1,maxDegree+1)*amplitude_zero;
+Selection_Z.bc(3).coefficient = ones(maxOrder+1,maxDegree+1)*amplitude_zero;
+Selection_Z.bs(1).coefficient = ones(maxOrder+1,maxDegree+1)*amplitude_zero;
+Selection_Z.bs(2).coefficient = ones(maxOrder+1,maxDegree+1)*amplitude_zero;
+Selection_Z.bs(3).coefficient = ones(maxOrder+1,maxDegree+1)*amplitude_zero;
 
 Selection_Z.bc(1).coefficient(2,2) = coefGradient;
 Selection_Z.bs(2).coefficient(2,2) = coefGradient*2;
@@ -36,12 +36,12 @@ Selection_Z.current = 1;
 Selection_Z.rhoReference = rhoReference;
 %% Drive X
 disp('Define the Drive X')
-Drive_X.bc(1).coefficient = ones(maxOrder,maxDegree)*amplitude_zero;
-Drive_X.bc(2).coefficient = ones(maxOrder,maxDegree)*amplitude_zero;
-Drive_X.bc(3).coefficient = ones(maxOrder,maxDegree)*amplitude_zero;
-Drive_X.bs(1).coefficient = ones(maxOrder,maxDegree)*amplitude_zero;
-Drive_X.bs(2).coefficient = ones(maxOrder,maxDegree)*amplitude_zero;
-Drive_X.bs(3).coefficient = ones(maxOrder,maxDegree)*amplitude_zero;
+Drive_X.bc(1).coefficient = ones(maxOrder+1,maxDegree+1)*amplitude_zero;
+Drive_X.bc(2).coefficient = ones(maxOrder+1,maxDegree+1)*amplitude_zero;
+Drive_X.bc(3).coefficient = ones(maxOrder+1,maxDegree+1)*amplitude_zero;
+Drive_X.bs(1).coefficient = ones(maxOrder+1,maxDegree+1)*amplitude_zero;
+Drive_X.bs(2).coefficient = ones(maxOrder+1,maxDegree+1)*amplitude_zero;
+Drive_X.bs(3).coefficient = ones(maxOrder+1,maxDegree+1)*amplitude_zero;
 
 %we set the current higher, in order to get a relatively small sensitivity
 Drive_X.current = 2330;
@@ -50,12 +50,12 @@ Drive_X.bc(1).coefficient(1,1) = coefDrive_X/Drive_X.current;
 Drive_X.rhoReference = rhoReference;
 %% Drive Y
 disp('Define the Drive Y')
-Drive_Y.bc(1).coefficient = ones(maxOrder,maxDegree)*amplitude_zero;
-Drive_Y.bc(2).coefficient = ones(maxOrder,maxDegree)*amplitude_zero;
-Drive_Y.bc(3).coefficient = ones(maxOrder,maxDegree)*amplitude_zero;
-Drive_Y.bs(1).coefficient = ones(maxOrder,maxDegree)*amplitude_zero;
-Drive_Y.bs(2).coefficient = ones(maxOrder,maxDegree)*amplitude_zero;
-Drive_Y.bs(3).coefficient = ones(maxOrder,maxDegree)*amplitude_zero;
+Drive_Y.bc(1).coefficient = ones(maxOrder+1,maxDegree+1)*amplitude_zero;
+Drive_Y.bc(2).coefficient = ones(maxOrder+1,maxDegree+1)*amplitude_zero;
+Drive_Y.bc(3).coefficient = ones(maxOrder+1,maxDegree+1)*amplitude_zero;
+Drive_Y.bs(1).coefficient = ones(maxOrder+1,maxDegree+1)*amplitude_zero;
+Drive_Y.bs(2).coefficient = ones(maxOrder+1,maxDegree+1)*amplitude_zero;
+Drive_Y.bs(3).coefficient = ones(maxOrder+1,maxDegree+1)*amplitude_zero;
 
 %we set the current higher, in order to get a relatively small sensitivity
 Drive_Y.current = 2330;
@@ -65,12 +65,12 @@ Drive_Y.rhoReference = rhoReference;
 
 %% Drive Z
 disp('Define the Drive Z')
-Drive_Z.bc(1).coefficient = ones(maxOrder,maxDegree)*amplitude_zero;
-Drive_Z.bc(2).coefficient = ones(maxOrder,maxDegree)*amplitude_zero;
-Drive_Z.bc(3).coefficient = ones(maxOrder,maxDegree)*amplitude_zero;
-Drive_Z.bs(1).coefficient = ones(maxOrder,maxDegree)*amplitude_zero;
-Drive_Z.bs(2).coefficient = ones(maxOrder,maxDegree)*amplitude_zero;
-Drive_Z.bs(3).coefficient = ones(maxOrder,maxDegree)*amplitude_zero;
+Drive_Z.bc(1).coefficient = ones(maxOrder+1,maxDegree+1)*amplitude_zero;
+Drive_Z.bc(2).coefficient = ones(maxOrder+1,maxDegree+1)*amplitude_zero;
+Drive_Z.bc(3).coefficient = ones(maxOrder+1,maxDegree+1)*amplitude_zero;
+Drive_Z.bs(1).coefficient = ones(maxOrder+1,maxDegree+1)*amplitude_zero;
+Drive_Z.bs(2).coefficient = ones(maxOrder+1,maxDegree+1)*amplitude_zero;
+Drive_Z.bs(3).coefficient = ones(maxOrder+1,maxDegree+1)*amplitude_zero;
 
 %we set the current higher, in order to get a relatively small sensitivity
 Drive_Z.current = 2330;
@@ -78,7 +78,7 @@ Drive_Z.bc(3).coefficient(1,1) = coefDrive_Z/Drive_Z.current;
 
 Drive_Z.rhoReference = rhoReference;
 
-clear('maxOrder','maxDegree','rhoReference',...
+clear('maxOrder+1','maxDegree+1','rhoReference',...
     'amplitude_zero','gradientInMainDirection','drivePeakAmplitude',...
     'coefGradient','coefDrive_X','coefDrive_Y','coefDrive_Z')
 
