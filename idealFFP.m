2da1e8dcee9071f875fd7aea1efd51a41a7cdedb
gBringout <gael.bringout@gmail.com>
2015-03-24 09:16:16 +0100

    Fix the selection coil definition

--- idealFFP.m
+++ idealFFP.m
@@ -11,15 +11,15 @@ rhoReference = 0.5;% [m] Fields are described inside a sphere of 1 meter of diam
 %log scale
 amplitude_zero = 10^-18;
 
-gradientInMainDirection = 5;
-drivePeakAmplitude = 0.050;
+gradientInMainDirection = 5.5;
+drivePeakAmplitude = 0.018;
 
 coefGradient = (gradientInMainDirection)*rhoReference;
 coefDrive_X = drivePeakAmplitude;
-coefDrive_Y = 2*drivePeakAmplitude;
+coefDrive_Y = drivePeakAmplitude;
 coefDrive_Z = drivePeakAmplitude;
 
-%% Delection
+%% Selection
 disp('Define the Selection coil')
 Selection_Z.bc(1).coefficient = ones(maxOrder+1,maxDegree+1)*amplitude_zero;
 Selection_Z.bc(2).coefficient = ones(maxOrder+1,maxDegree+1)*amplitude_zero;
@@ -28,9 +28,9 @@ Selection_Z.bs(1).coefficient = ones(maxOrder+1,maxDegree+1)*amplitude_zero;
 Selection_Z.bs(2).coefficient = ones(maxOrder+1,maxDegree+1)*amplitude_zero;
 Selection_Z.bs(3).coefficient = ones(maxOrder+1,maxDegree+1)*amplitude_zero;
 
-Selection_Z.bc(1).coefficient(2,2) = coefGradient;
-Selection_Z.bs(2).coefficient(2,2) = coefGradient*2;
-Selection_Z.bc(3).coefficient(2,1) = coefGradient;
+Selection_Z.bc(1).coefficient(2,2) = coefGradient/2;
+Selection_Z.bs(2).coefficient(2,2) = coefGradient/2;
+Selection_Z.bs(3).coefficient(2,1) = coefGradient;
 
 Selection_Z.current = 1;
 Selection_Z.rhoReference = rhoReference;
@@ -44,7 +44,7 @@ Drive_X.bs(2).coefficient = ones(maxOrder+1,maxDegree+1)*amplitude_zero;
 Drive_X.bs(3).coefficient = ones(maxOrder+1,maxDegree+1)*amplitude_zero;
 
 %we set the current higher, in order to get a relatively small sensitivity
-Drive_X.current = 2330;
+Drive_X.current = 200;
 Drive_X.bc(1).coefficient(1,1) = coefDrive_X/Drive_X.current;
 
 Drive_X.rhoReference = rhoReference;
@@ -58,7 +58,7 @@ Drive_Y.bs(2).coefficient = ones(maxOrder+1,maxDegree+1)*amplitude_zero;
 Drive_Y.bs(3).coefficient = ones(maxOrder+1,maxDegree+1)*amplitude_zero;
 
 %we set the current higher, in order to get a relatively small sensitivity
-Drive_Y.current = 2330;
+Drive_Y.current = 250;
 Drive_Y.bc(2).coefficient(1,1) = coefDrive_Y/Drive_Y.current;
 
 Drive_Y.rhoReference = rhoReference;
@@ -73,7 +73,7 @@ Drive_Z.bs(2).coefficient = ones(maxOrder+1,maxDegree+1)*amplitude_zero;
 Drive_Z.bs(3).coefficient = ones(maxOrder+1,maxDegree+1)*amplitude_zero;
 
 %we set the current higher, in order to get a relatively small sensitivity
-Drive_Z.current = 2330;
+Drive_Z.current = 150;
 Drive_Z.bc(3).coefficient(1,1) = coefDrive_Z/Drive_Z.current;
 
 Drive_Z.rhoReference = rhoReference;
